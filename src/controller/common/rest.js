53befd31c13effe022ef9bd5860c15bed2796cd6
yuzhuo li <yuzhuoli@yuzhuos-MacBook-Pro.local>
2017-12-13 16:09:42 +0800

    fix restfulapi CORS issue

--- src/controller/common/rest.js
+++ src/controller/common/rest.js
@@ -85,6 +85,20 @@ module.exports = class extends think.Controller {
     return this.success({affectedRows: rows});
   }
   __call() {
+    let method = this.http.method.toLowerCase();
+    if(method === "options"){
+      this.setCorsHeader();
+      this.end();
+      return;
+    }
+    this.setCorsHeader();
+    return super.__call();
+  }
 
+  setCorsHeader(){
+    this.header("Access-Control-Allow-Origin", this.header("origin") || "*");
+    this.header("Access-Control-Allow-Headers", "x-requested-with");
+    this.header("Access-Control-Request-Method", "GET,POST,PUT,DELETE");
+    this.header("Access-Control-Allow-Credentials", "true");
   }
 };
